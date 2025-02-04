7c368542aa1f89e61439ff4f53042ff3cc817fd4
miraaanuar <2023372091@student.uitm.edu.my>
2025-02-05 00:24:25 +0800

    update 5/2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,3 +47,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
 }
+
+dependencies {
+    implementation ("androidx.cardview:cardview:1.0.0")
+}
